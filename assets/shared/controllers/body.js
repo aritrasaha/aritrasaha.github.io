731d013f4b1b20fadefe1ea92202b1ab644a29bb
aritrasaha <aritrasaha.90@gmail.com>
2015-03-31 15:38:08 +0530

    testing js crawling

--- assets/shared/controllers/body.js
+++ assets/shared/controllers/body.js
@@ -2,6 +2,6 @@ define([ '../app' ], function (module) {
     module.controller('BodyController',
     [ '$scope',
     function ($scope) {
-        
+        $scope.name = "Aritra Saha"
     } ]);
 });
